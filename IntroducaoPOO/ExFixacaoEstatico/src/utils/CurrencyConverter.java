f97d4f8875fefe5f3a7cd14a3b57ee2e537a8f00
Hugo Ribeiro <hugolerib@gmail.com>
2021-05-17 19:52:42 -0300

    Exercicio metodo estatico

--- IntroducaoPOO/ExFixacaoEstatico/src/utils/CurrencyConverter.java
+++ IntroducaoPOO/ExFixacaoEstatico/src/utils/CurrencyConverter.java
@@ -5,7 +5,7 @@ public class CurrencyConverter {
     private static double IOF = 0.06;
 
     public static double dollarToReal(double amount, double dollarPrice){
-        return amount * dollarPrice * (1 + IOF);
+        return amount * dollarPrice - (IOF * amount * dollarPrice);
     }
 
 }
